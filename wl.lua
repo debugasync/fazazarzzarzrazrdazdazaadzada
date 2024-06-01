97d9fe8058ad0245f8a6424ac8c29058e2638b19
farzad <87902064+debugasync@users.noreply.github.com>
2024-06-02 00:26:26 +0200

    Create wl.lua

---
+++ wl.lua
@@ -0,0 +1,37 @@
+local Licenses = {
+    "void-4Njkx-NDAO2-9xaFb-81x23",
+    "void-3FghT-2JKlM-7iOqP-9rStU"
+}
+
+local UserIds = {
+    "1",
+    "2"
+}
+
+local Password = {
+    "FarzadPrivateRageTownVeryVerySecurePasswortAlmostLike3000000DigitsLolCapIkTrolEmirWasHereHaHAHVerySecure"
+}
+
+local TwoFA = {
+    45143421
+}
+
+local GayMeter = {
+    "Max"
+}
+
+local ViceWeakyMtr = {
+    "lowwwww"
+}
+
+
+local whitelistData = {
+    Licenses = Licenses,
+    UserIds = UserIds,
+    Password = Password,
+    TwoFA = TwoFA,
+    GayMeter = GayMeter,
+    ViceWeakyMtr = ViceWeakyMtr
+}
+
+return whitelistData
